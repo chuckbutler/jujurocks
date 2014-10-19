2db5b0348c64f32a21a41b3780649a8df6d0f190
Charles Butler <chuck@dasroot.net>
2014-10-19 02:43:44 -0400

    Added get 200 ok tests for player and home controllers

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
-
+  context "GET" do
+    it "renders the index view" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 end
